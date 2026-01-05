41482c4989ad6e36317398a34d5e80da3c645d53
guart58 <80782747+guart58@users.noreply.github.com>
2026-01-06 01:39:49 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+settings = {
+    'token': token,
+    'bot': 'Ulyana bulbasavr',
+    'id': 1457730874324815905,
+    'prefix': '?',
+    'api_key': 'live_ggKGZtEwseA2BONyQBmGwrZHGgKxKUtLz4uaaMzLZFefoUT7utXV2KVM61kD4jld'
+}
